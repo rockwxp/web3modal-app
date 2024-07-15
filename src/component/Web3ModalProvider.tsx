935b6cecd1412dfe8ac5b5416d2001eb9e5a7d59
rock <rockwxp@163.com>
2024-07-15 22:30:46 +0800

    Initial commit

---
+++ src/component/Web3ModalProvider.tsx
@@ -0,0 +1,44 @@
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { WagmiProvider } from "wagmi";
+import { arbitrum, mainnet, sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId from https://cloud.walletconnect.com
+const projectId = process.env.REACT_APP_PROJECT_ID as string;
+if (projectId) throw new Error("REACT_APP_PROJECT_ID not found");
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum, sepolia] as const;
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
+export default function Web3ModalProvider() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
+    </WagmiProvider>
+  );
+}
